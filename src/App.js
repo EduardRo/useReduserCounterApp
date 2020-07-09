89d9377b986e02561eb847505f5a9621fa1e9009
Eduard Stancu <eduardstancu@yahoo.com>
2020-07-09 09:49:05 +0300

    reducer modify

--- src/App.js
+++ src/App.js
@@ -1,38 +1,65 @@
 import React, { useState, useReducer } from 'react';
 
 import './App.css';
+const ACTION = {
+  INCREMENT: 'INCREMENT',
+  DECREMENT: 'DECREMENT',
+  SPUNE: 'SPUNE',
+};
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'increment':
+    case ACTION.INCREMENT:
       return { count: state.count + 1 };
-    case 'decrement':
+    case ACTION.DECREMENT:
       return { count: state.count - 1 };
+    case ACTION.SPUNE:
+      return { spune: state.spune + 'ddd' };
     default:
       return state;
   }
 }
 
+// function reducer
+
+// function App()
+
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const [state, dispatch] = useReducer(reducer, [
+    { count: 0 },
+    { spune: 'abc' },
+  ]);
 
   function increment() {
-    dispatch({ type: 'increment' });
+    dispatch({ type: ACTION.INCREMENT, count });
   }
 
   function decrement() {
-    dispatch({ type: 'decrement' });
+    dispatch({ type: ACTION.DECREMENT, count });
+  }
+
+  function spune() {
+    dispatch({ type: ACTION.SPUNE, spune });
   }
 
   return (
-    <div className='App'>
-      <button onClick={decrement} className='button'>
-        <span>-</span>
-      </button>
-      <span>{state.count}</span>
-      <button onClick={increment} className='button'>
-        <span>+</span>
-      </button>
+    <div>
+      <div className='App'>
+        <button onClick={decrement} className='button'>
+          <span>-</span>
+        </button>
+        <span>{state.count}</span>
+
+        <button onClick={increment} className='button'>
+          <span>+</span>
+        </button>
+      </div>
+      <div>
+        <button onClick={spune} className='button'>
+          Spune
+        </button>
+        <span>{state.spune}</span>
+      </div>
     </div>
   );
 }
